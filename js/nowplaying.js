af10ff5240316033e125889b00ee0eff55854be3
Your Name <you@example.                com>
2017-02-12 10:21:25 +0800

    实现跨域请求的封装

--- js/nowplaying.js
+++ js/nowplaying.js
@@ -1,5 +1,29 @@
 //创建一个控制器
 angular.module("movieApp.nowplayingCtrl",[])
 .controller("nowplayingCtrl",["$scope","$movieServ",function($scope,$movieServ){
+	//实现电影列表展示功能
+	$scope.movie = {};
 
+	//通过ajax请求data.json中的数据,将它赋值给$scope.movie
+	//$http就是帮我们实现ajax请求的,和jq中的ajax方式有点不一样
+	/*$http({
+		url:"data.json",
+	}).success(function(data){
+			console.log(data);
+			$scope.movie = data;
+		})*/
+		/*jsonp('https://api.douban.com/v2/movie/in_theaters', {
+        count: 1,
+        start: 0
+    }, function(data) {
+        console.log(data);
+    })*/
+
+	$movieServ.jsonp('https://api.douban.com/v2/movie/in_theaters', {
+        count: 20,
+        start: 0
+    }, function(data) {
+        $scope.movie = data;
+        $scope.$apply();
+    })
 }])
--- js/route.js
+++ js/route.js
@@ -1,5 +1,20 @@
 //创建一个路由模块
 angular.module("movieApp.route",["ngRoute"])
 .config(["$routeProvider",function($routeProvider){
-	
+	//配置路由
+	$routeProvider.when("/",{
+		templateUrl : "tmps/index-tmp.html"
+	})
+	.when("/later",{
+		templateUrl : "tmps/later-tmp.html",
+		controller : "laterCtrl"
+	})
+	.when("/top250",{
+		templateUrl : "tmps/Top250-tmp.html",
+		controller : "Top250Ctrl"
+	})
+	.when("/nowplaying",{
+		templateUrl : "tmps/nowplaying-tmp.html",
+		controller : "nowplayingCtrl"
+	})
 }])
